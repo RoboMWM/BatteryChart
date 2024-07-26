0db4edcf9002a75b58b782e2a2322f304ffc7d80
RoboMWM <robomwm@users.noreply.github.com>
2024-07-25 23:52:49 -0700

    add apply events button and logic

--- BatteryChart/BatteryChart/SettingsPage.xaml.cs
+++ BatteryChart/BatteryChart/SettingsPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -24,6 +26,23 @@ namespace BatteryChart
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        private BackgroundAccessStatus backgroundAccessStatus;
+
+        private async Task<bool> IsBackgroundAccessAllowed()
+        {
+            if (this.backgroundAccessStatus != BackgroundAccessStatus.Unspecified)
+                this.backgroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
+
+            switch (this.backgroundAccessStatus)
+            {
+                case BackgroundAccessStatus.DeniedBySystemPolicy:
+                case BackgroundAccessStatus.DeniedByUser:
+                    return false; //TODO: error message(?)
+                default:
+                    return true;
+            }
+        }
+
         public SettingsPage()
         {
             //TODO: load from file
@@ -31,39 +50,80 @@ namespace BatteryChart
             SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
         }
 
-        private async void RegisterBackgroundTask()
+        private async Task<bool> RegisterBackgroundTask(params SystemTrigger[] triggers)
         {
-            switch (await BackgroundExecutionManager.RequestAccessAsync())
-            {
-                case BackgroundAccessStatus.DeniedBySystemPolicy:
-                case BackgroundAccessStatus.DeniedByUser:
-                    return; //TODO: error message
-                default:
-                    break;
-            }
+            if (!await IsBackgroundAccessAllowed())
+                return false;
 
             //Unregister all tasks
+            //Will not be viable when we start registering different tasks for updating the tile
             foreach (KeyValuePair<Guid, IBackgroundTaskRegistration> task in BackgroundTaskRegistration.AllTasks)
                 task.Value.Unregister(true);
 
-            //Register new background task exclusively for PowerStateChange
-            //TODO implement other triggers here or in another method?
-            BackgroundTaskBuilder taskBuilder = new BackgroundTaskBuilder();
-            taskBuilder.Name = "LogAndUpdateTile";
-            taskBuilder.TaskEntryPoint = "BackgroundTasks.LogAndUpdateTile";
-            taskBuilder.SetTrigger(new SystemTrigger(SystemTriggerType.PowerStateChange, false));
-            BackgroundTaskRegistration registration = taskBuilder.Register();
+            //Register each trigger
+            foreach (SystemTrigger trigger in triggers)
+            {
+                BackgroundTaskBuilder taskBuilder = new BackgroundTaskBuilder();
+                taskBuilder.Name = trigger.TriggerType.ToString();
+                taskBuilder.TaskEntryPoint = "BackgroundTasks.LogBatteryToFileTask";
+                taskBuilder.SetTrigger(trigger);
+                taskBuilder.Register();
+            }
+
+            return true;
         }
 
-        private void SaveAppBarButtonClick(object sender, RoutedEventArgs e)
+        private async void TriggersComboBox_Loaded(object sender, RoutedEventArgs e)
         {
-            //TODO: how to handle button spam? Maybe show messagedialog confirming changes?
-            RegisterBackgroundTask();
+            if (!await IsBackgroundAccessAllowed())
+            {
+                TriggersComboBox.PlaceholderText = "Background access denied";
+                return;
+            }
+
+            //All this to avoid a config file
+            bool power = false;
+            bool userPresentAway = false;
+
+            foreach (KeyValuePair<Guid, IBackgroundTaskRegistration> task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == SystemTriggerType.PowerStateChange.ToString())
+                    power = true;
+                else if (task.Value.Name == SystemTriggerType.UserPresent.ToString() || task.Value.Name == SystemTriggerType.UserAway.ToString())
+                    userPresentAway = true;
+            }
+
+            if (power && userPresentAway)
+                TriggersComboBox.SelectedItem = "All Events";
+            else if (power)
+                TriggersComboBox.SelectedItem = "Only plug/unplug";
+            else if (userPresentAway)
+                TriggersComboBox.SelectedItem = "Only wake/sleep";
+            else
+                TriggersComboBox.SelectedItem = "None";
         }
 
-        private void TriggersComboBox_Loaded(object sender, RoutedEventArgs e)
+        private async void ApplyButton_Click(object sender, RoutedEventArgs e)
         {
-            TriggersComboBox.SelectedItem = "Only plug/unplug";
+            ApplyProgressRing.IsActive = true;
+
+            switch (TriggersComboBox.SelectedItem)
+            {
+                case "None":
+                    if (!await RegisterBackgroundTask())
+                        return;
+                    break;
+                case "Only plug/unplug":
+                    if (!await RegisterBackgroundTask(new SystemTrigger(SystemTriggerType.PowerStateChange, false)))
+                        return; //TODO: print error and clear progress ring
+                    break;
+                case "Only wake/sleep":
+                    if (!await RegisterBackgroundTask(new SystemTrigger(SystemTriggerType.UserPresent, false), new SystemTrigger(SystemTriggerType.UserAway, false)))
+                        return; //TODO: print error and clear progress ring
+                    break;
+            }
+
+            ApplyProgressRing.IsActive = false;
         }
     }
 }
