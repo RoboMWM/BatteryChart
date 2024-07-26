3194b55c30d180f0d2f2a9f8206695447ceaf8ae
RoboMWM <robomwm@users.noreply.github.com>
2024-07-25 22:33:12 -0700

    rename to SettingsPage for consistency

--- BatteryChart/BatteryChart/SettingsPage.xaml.cs
+++ BatteryChart/BatteryChart/SettingsPage.xaml.cs
@@ -22,9 +22,9 @@ namespace BatteryChart
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class OptionsPage : Page
+    public sealed partial class SettingsPage : Page
     {
-        public OptionsPage()
+        public SettingsPage()
         {
             //TODO: load from file
             this.InitializeComponent();
