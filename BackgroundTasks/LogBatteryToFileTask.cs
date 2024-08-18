276871e5c1b69afef603bb25f30f986ff7e394a7
RoboMWM <robomwm@users.noreply.github.com>
2024-08-17 17:55:50 -0700

    Revert "start skeleton of future code sharing thingything"
    This reverts commit 7ac638f7b3faa0fdf0e7a7b7760d3be0426994a5.

--- BackgroundTasks/LogBatteryToFileTask.cs
+++ BackgroundTasks/LogBatteryToFileTask.cs
@@ -1,5 +1,4 @@
-﻿using BatteryChart;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,9 +36,33 @@ namespace BackgroundTasks
             sb.Append(",");
             sb.AppendLine(taskInstance.Task.Name.ToString());
 
-            await LogFileStuff.WriteToFile("BatteryLog.csv", sb.ToString());
+            await WriteToFile("BatteryLog.csv", sb.ToString());
 
             deferral.Complete();
+        }
+
+        private async Task WriteToFile(string fileName, string content)
+        {
+            try
+            {
+                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+                StorageFile file = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+
+                try
+                {
+                    await FileIO.AppendTextAsync(file, content);
+                }
+                catch (Exception e)
+                {
+                    StorageFile errorFile = await localFolder.CreateFileAsync("Err-" + fileName, CreationCollisionOption.OpenIfExists);
+                    await FileIO.AppendTextAsync(errorFile, content);
+                }
+            }
+
+            catch (Exception e)
+            {
+                //Nothing for now TODO: error log?
+            }
         }
     }
 }
